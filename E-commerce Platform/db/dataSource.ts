474f0b0e93a7ac2a09d5c4b7e8de601b87a90931
Ansam Nidal Mousa Abu Al Jadayel <137777479+Ansam-02@users.noreply.github.com>
2023-10-07 17:00:00 +0300

    install required depandancies

--- E-commerce Platform/db/dataSource.ts
+++ E-commerce Platform/db/dataSource.ts
@@ -16,10 +16,10 @@ const dataSource = new DataSource({
   type: 'mysql',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  // username: process.env.DB_USER,
-  //
-  username: 'root',
-  //
+  username: process.env.DB_USER,
+  
+  
+  
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: [
